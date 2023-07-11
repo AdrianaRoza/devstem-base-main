1ee3e6c16b9dc1bb42fcdca08abb0389f7cb2c66
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-07-11 14:24:28 -0400

    refactor: color and logo change

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>DevSteam: A sua loja online de games</title>
         <meta name="description" content="DevSteam: A sua loja online de games" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/video-game.gif" />
       </Head>
       <div>
         <Navbar />
