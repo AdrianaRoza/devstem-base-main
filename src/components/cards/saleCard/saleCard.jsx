8572ed43eecca706f243d1b2f7847a4fedd42712
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-07-16 11:03:46 -0400

    refactor: adding parameters to reuse them and add images

--- src/components/cards/saleCard/saleCard.jsx
+++ src/components/cards/saleCard/saleCard.jsx
@@ -3,21 +3,28 @@ import Image from 'next/image'
 import style from './saleCard.module.css'
 import  Button from '@/components/forms/button/button'
 
-export default function SaleCard() {
+export default function SaleCard({ image, discount, fullPrice,discountPrice} ) {
   return(
     <div className={style.salecard}>
-      <Image src='/products/league-of-legends.jpg' width={220} height={300} />
+      <Image src={`/products/${image}`} alt={`Produto ${image}`} width={220} height={300} />
       <div className={style.info}>
         <h3 className={style.title}>Oferta Exclusiva</h3>
         <div className={style.pricecard}>
-          <div className={style.percent}>-20%</div>
+          <div className={style.percent}>-{discount}</div>
           <div className={style.price}>
-            <p className={style.fullprice}>R$99,90</p>
-            <h4 className={style.discountprice}>R$89,90</h4>
+            <p className={style.fullprice}>{fullPrice}</p>
+            <h4 className={style.discountprice}>{discountPrice}</h4>
           </div>
         </div>
         <Button fullwidth>Adicionar ao carrinho</Button>
       </div>
     </div>
   )
+}
+
+SaleCard.defaultProps = {
+  image: 'league-of-legends.jpg',
+  discount:'90%',
+  fullPrice:'199,90',
+  discountPrice: '99,00',
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,9 +23,46 @@ export default function Home() {
           <div className={styles.session}>
             <Subtitle>Promoções</Subtitle>
             <div className={styles.salecontainer}>
-              <SaleCard />
-              <SaleCard />
-              <SaleCard />
+              <SaleCard
+              image={'league-of-legends.jpg'}
+              discount='30%'
+              fullPrice='40,90'
+              discountPrice= '12,27'
+               />
+
+              <SaleCard 
+              image={'game4.jpg'}
+              discount='50%'
+              fullPrice='70,90'
+              discountPrice= '35,25'
+              />
+
+              <SaleCard 
+              image={'valorant.jpg'}
+              discount='30%'
+              fullPrice='55,60'
+              discountPrice= '16,68 '
+              />
+
+              <SaleCard 
+              image={'game2.jpg'}
+              discount='10%'
+              fullPrice='80,60'
+              discountPrice= '24,18'
+              />
+              <SaleCard 
+              image={'dota-2.jpg'}
+              discount='30%'
+              fullPrice='90,00'
+              discountPrice= '27,00'
+              />
+              <SaleCard 
+              image={'game5.jpg'}
+              discount='50%'
+              fullPrice='100,00'
+              discountPrice= '50,00 '
+              />
+
             </div>
           </div>
           <div className={styles.session}>
